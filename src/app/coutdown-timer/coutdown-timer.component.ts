3961d479c3508a9c20d56b4d92092efb6efd19c0
Anuswan91 <vivien.blum@free.fr>
2018-03-14 23:59:09 +0100

    [Answer] Time to 10

--- src/app/coutdown-timer/coutdown-timer.component.ts
+++ src/app/coutdown-timer/coutdown-timer.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, Output, EventEmitter } from "@angular/core"
   styleUrls: ["./coutdown-timer.component.css"]
 })
 export class CoutdownTimerComponent implements OnInit {
-  static TIME = 3
+  static TIME = 10
   intervalId = 0
   seconds = CoutdownTimerComponent.TIME
 
