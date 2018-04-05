ce4d158a2ecff5921f974db27180b5b11f27a0d9
Anuswan91 <vivien.blum@free.fr>
2018-04-05 09:53:46 +0200

    Service picks random questions

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,49 +1,60 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core"
 import { Game } from "../models/game"
+import { Question } from "../models/question"
 
 @Injectable()
 export class GameService {
+  questions: Question[] = [
+    {
+      id: 1,
+      question: "A quel animal l’adjectif «hippique » se rapporte-t-il ?",
+      answers: [
+        { answer: "Au canard", isTrue: false },
+        { answer: "Au cheval", isTrue: true },
+        { answer: "Au chien", isTrue: false },
+        { answer: "Au hibou", isTrue: false }
+      ]
+    },
+    {
+      id: 2,
+      question:
+        "En quelle année Jacques Chirac dissout-il l'assemblée nationale?",
+      answers: [
+        { answer: "1997", isTrue: true },
+        { answer: "1999", isTrue: false },
+        { answer: "1994", isTrue: false },
+        { answer: "1996", isTrue: false }
+      ]
+    },
+    {
+      id: 3,
+      question:
+        "Comment s'appelait le personnage qui jouait du Synthé dans la série Hélène et les garçons?",
+      answers: [
+        { answer: "Mat", isTrue: false },
+        { answer: "José", isTrue: true },
+        { answer: "Alex", isTrue: false },
+        { answer: "Nicolas", isTrue: false }
+      ]
+    }
+  ]
 
-  constructor() { }
+  constructor() {}
+
+  protected getQuestions(nb:number): Question[] {
+    const array = []
+    for(let i = 0; i < nb; i++) {
+      array.push(this.questions[Math.floor(Math.random() * this.questions.length)])
+    }
+    return array
+  }
 
   public getGame(): Game {
     const game = {
       id: 1,
       score: 0,
-      questions: [
-        {
-          id: 1,
-          question: "A quel animal l’adjectif «hippique » se rapporte-t-il ?",
-          answers: [
-            { answer: "Au canard", isTrue: false },
-            { answer: "Au cheval", isTrue: true },
-            { answer: "Au chien", isTrue: false },
-            { answer: "Au hibou", isTrue: false }
-          ]
-        },
-        {
-          id: 2,
-          question: "En quelle année Jacques Chirac dissout-il l'assemblée nationale?",
-          answers: [
-            { answer: "1997", isTrue: true },
-            { answer: "1999", isTrue: false },
-            { answer: "1994", isTrue: false },
-            { answer: "1996", isTrue: false }
-          ]
-        },
-        {
-          id: 3,
-          question: "Comment s'appelait le personnage qui jouait du Synthé dans la série Hélène et les garçons?",
-          answers: [
-            { answer: "Mat", isTrue: false },
-            { answer: "José", isTrue: true },
-            { answer: "Alex", isTrue: false },
-            { answer: "Nicolas", isTrue: false }
-          ]
-        }
-      ]
+      questions: this.getQuestions(1)
     }
     return game
   }
-
 }
