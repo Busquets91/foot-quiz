54b4a8e94a9982c4edee1cb92cf22cb710fffab7
Anuswan91 <vivien.blum@free.fr>
2018-04-05 10:14:41 +0200

    Added data questions

---
+++ src/app/data/questions.ts
@@ -0,0 +1,36 @@
+const questions = [
+  {
+    id: 1,
+    question: "A quel animal l’adjectif «hippique » se rapporte-t-il ?",
+    answers: [
+      { answer: "Au canard", isTrue: false },
+      { answer: "Au cheval", isTrue: true },
+      { answer: "Au chien", isTrue: false },
+      { answer: "Au hibou", isTrue: false }
+    ]
+  },
+  {
+    id: 2,
+    question:
+      "En quelle année Jacques Chirac dissout-il l'assemblée nationale?",
+    answers: [
+      { answer: "1997", isTrue: true },
+      { answer: "1999", isTrue: false },
+      { answer: "1994", isTrue: false },
+      { answer: "1996", isTrue: false }
+    ]
+  },
+  {
+    id: 3,
+    question:
+      "Comment s'appelait le personnage qui jouait du Synthé dans la série Hélène et les garçons?",
+    answers: [
+      { answer: "Mat", isTrue: false },
+      { answer: "José", isTrue: true },
+      { answer: "Alex", isTrue: false },
+      { answer: "Nicolas", isTrue: false }
+    ]
+  }
+]
+
+export default questions
--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -2,50 +2,19 @@ import { Injectable } from "@angular/core"
 import { Game } from "../models/game"
 import { Question } from "../models/question"
 import { GameConfig } from "../models/game-config"
+import questions from "../data/questions"
 
 @Injectable()
 export class GameService {
-  questions: Question[] = [
-    {
-      id: 1,
-      question: "A quel animal l’adjectif «hippique » se rapporte-t-il ?",
-      answers: [
-        { answer: "Au canard", isTrue: false },
-        { answer: "Au cheval", isTrue: true },
-        { answer: "Au chien", isTrue: false },
-        { answer: "Au hibou", isTrue: false }
-      ]
-    },
-    {
-      id: 2,
-      question:
-        "En quelle année Jacques Chirac dissout-il l'assemblée nationale?",
-      answers: [
-        { answer: "1997", isTrue: true },
-        { answer: "1999", isTrue: false },
-        { answer: "1994", isTrue: false },
-        { answer: "1996", isTrue: false }
-      ]
-    },
-    {
-      id: 3,
-      question:
-        "Comment s'appelait le personnage qui jouait du Synthé dans la série Hélène et les garçons?",
-      answers: [
-        { answer: "Mat", isTrue: false },
-        { answer: "José", isTrue: true },
-        { answer: "Alex", isTrue: false },
-        { answer: "Nicolas", isTrue: false }
-      ]
-    }
-  ]
 
   constructor() {}
 
-  protected getQuestions(nb:number): Question[] {
+  protected getQuestions(nb: number): Question[] {
     const array = []
-    for(let i = 0; i < nb; i++) {
-      array.push(this.questions[Math.floor(Math.random() * this.questions.length)])
+    for (let i = 0; i < nb; i++) {
+      array.push(
+        questions[Math.floor(Math.random() * questions.length)]
+      )
     }
     return array
   }
